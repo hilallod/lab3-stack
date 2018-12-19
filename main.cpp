550dd0d5b2f81298efd1e709511745fbb7d76d08
Широков Сергей Александрович <shirokov.s@ccam.cmc.unn.net>
2018-12-19 15:31:56 +0300

    qwer

--- main.cpp
+++ main.cpp
@@ -1,32 +1,15 @@
 #include "Tcalc.h"
 #include <iostream>
-using namespace std;
 
 void main()
 {
 	TCalculator calc;
 	string example;
-	int i = 1, j;
-	while (i == 1)
-	{
-		system("cls");
-		cout << "1.Calc" << endl;
-		cout << "2.Exit" << endl;
-		cin >> j;
-		if (j == 1)
-		{
-			cout << "Enter exp" << endl;
-			cin >> example;
-			calc.SetInf(example);
-			calc.ToPost();
-			cout << calc.Calcucate() << endl;
-			system("pause");
-			j = 0;
-		}
-		if (j==2)
-			return;
-	}
 
+	cout << "Enter exp" << endl;
+	cin >> example;
+
+	calc.SetInf(example);
+	calc.ToPost();
+	cout << calc.Calcucate() << endl;
 }
-
-
